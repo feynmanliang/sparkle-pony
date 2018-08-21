b3a448a4a64c26812a9fab9c343511b0d472e026
Feynman Liang <feynman.liang@gmail.com>
2018-08-21 11:07:48 -0700

    Adds working grpc example

---
+++ server/README.md
@@ -0,0 +1,18 @@
+# SparklePony Backend
+
+Uses grpc-gateway to provide a type-safe HTTP/gRPC API.
+
+## Compiling gRPC protos
+
+```
+# gRPC protoc plugin
+go get -u google.golang.org/grpc
+
+# Generate Go code from protobufs
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+
+cd proto && ninja
+```
---
+++ server/proto/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	pb ".."
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Sending reply to %s", in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
